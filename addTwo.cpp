b0dd9b6560d8d54b7e72e577f5dc2f95e538171c
Shubham Kant Dubey <shubham.kant.dubey@gmail.com>
2020-02-18 20:53:32 +0530

    Update addTwo.cpp

--- addTwo.cpp
+++ addTwo.cpp
@@ -20,3 +20,65 @@ int main()
   
   cout << "sum of two integers = " << sumOfTwoInt(num1,num2);
 }
+#include <stdio.h>
+
+void func1();
+void func2();
+void func3();
+void func4();
+
+typedef struct __context_t context_t;
+typedef (*functionHandler)(context_t*);
+
+struct __context_t
+{
+    functionHandler *funcHandler;
+};
+
+
+const unsigned long int handler[2][2] = 
+{
+    (unsigned long int)func1,
+    (unsigned long int)func2,
+    (unsigned long int)func3,
+    (unsigned long int)func4,
+};
+
+static context_t a = {
+                    (functionHandler *)handler,
+                  };
+
+
+
+void func1(context_t*d)
+{
+    printf("1\n");
+}
+
+void func2(context_t*d)
+{
+    printf("2\n");
+}
+
+void func3(context_t* d)
+{
+    printf("3\n");
+}
+
+void func4(context_t* d)
+{
+    printf("4\n");
+}
+
+int main()
+{
+    context_t* c =&a;
+    functionHandler z=func1;
+    
+    //void (*fun_ptr)(context_t*) = func1;
+    (z)(c);
+    (*c->funcHandler)(c);
+    
+
+    return 0;
+}
